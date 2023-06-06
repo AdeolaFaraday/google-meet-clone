4012a5ef9247fe685ca5b27194d3ee325a821c76
AdeolaFaraday <adexconly@gmail.com>
2023-06-06 11:48:23 -0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Home from "./screen/Home.vue";
+</script>
+
+<template>
+  <Home />
+</template>
+
+<style scoped></style>
+
+<!-- https://docs.agora.io/en/video-calling/develop/product-workflow?platform=android -->
---
+++ src/screen/Home.vue
@@ -0,0 +1,19 @@
+<script setup></script>
+<template>
+  <div>
+    <button type="button" id="inItScreen">Share Screen</button>
+    <button type="button" id="muteVideo">Mute Video</button>
+    <br /><label> Local Audio Level :</label>
+    <input
+      type="range"
+      min="0"
+      id="localAudioVolume"
+      max="100"
+      step="1"
+    /><br />
+    <label> Remote Audio Level :</label>
+    <input type="range" min="0" id="remoteAudioVolume" max="100" step="1" />
+  </div>
+</template>
+
+<style scoped></style>
