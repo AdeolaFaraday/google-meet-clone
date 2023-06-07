866d2790e4cdd2de7c485f9a0101a802681f480f
AdeolaFaraday <adexconly@gmail.com>
2023-06-07 10:57:26 -0700

    implement local screen sharing logic

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,19 @@
 <script setup>
 import Home from "./screen/Home.vue";
+import AgoraRTC from "agora-rtc-sdk-ng";
+
+AgoraRTC.onAutoplayFailed = () => {
+  // Create button for the user interaction.
+  const btn = document.createElement("button");
+  // Set the button text.
+  btn.innerText = "Click me to resume the audio/video playback";
+  // Remove the button when onClick event occurs.
+  btn.onClick = () => {
+    btn.remove();
+  };
+  // Append the button to the UI.
+  document.body.append(btn);
+};
 </script>
 
 <template>
@@ -8,4 +22,4 @@ import Home from "./screen/Home.vue";
 
 <style scoped></style>
 
-<!-- https://docs.agora.io/en/video-calling/develop/product-workflow?platform=android -->
+<!-- https://docs.agora.io/en/video-calling/develop/product-workflow?platform=web -->
--- src/screen/Home.vue
+++ src/screen/Home.vue
@@ -1,7 +1,98 @@
-<script setup></script>
+<script setup>
+import { onMounted, reactive, ref } from "vue";
+import AgoraRTC from "agora-rtc-sdk-ng";
+
+let isSharingEnabled = ref(false);
+let isMuteVideo = ref(false);
+
+const localVideo = ref(null);
+const localVideoStream = ref(null);
+
+let options = reactive({
+  // Pass your App ID here.
+  appId: "",
+  // Set the channel name.
+  channel: "",
+  // Pass your temp token here.
+  token: "",
+  // Set the user ID.
+  uid: 0,
+});
+
+let channelParameters = reactive({
+  // A variable to hold a local audio track.
+  screenTrack: null,
+  // A variable to hold a local video track.
+  localVideoTrack: null,
+  // A variable to hold a remote audio track.
+  remoteAudioTrack: null,
+  // A variable to hold a remote video track.
+  remoteVideoTrack: null,
+  // A variable to hold the remote user id.s
+  remoteUid: null,
+});
+
+async function initializeLocalVideoTrack() {
+  try {
+    const mediaStream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+    });
+
+    channelParameters.localVideoTrack = AgoraRTC.createCustomVideoTrack({
+      mediaStreamTrack: mediaStream.getVideoTracks()[0],
+    });
+
+    localVideoStream.value = mediaStream;
+    localVideo.value.srcObject = mediaStream;
+  } catch (error) {
+    console.error("Error accessing the camera:", error);
+  }
+}
+
+onMounted(() => {
+  initializeLocalVideoTrack();
+});
+
+const handleClick = async () => {
+  if (!isSharingEnabled.value) {
+    // Create a screen track for screen sharing.
+    channelParameters.screenTrack = await AgoraRTC.createScreenVideoTrack();
+    // Replace the video track with the screen track.
+    await channelParameters.localVideoTrack.replaceTrack(
+      channelParameters.screenTrack?.getMediaStreamTrack(),
+      true
+    );
+
+    localVideo.value.srcObject = new MediaStream([
+      channelParameters.screenTrack?.getMediaStreamTrack(),
+    ]);
+    // Update the button text.
+    document.getElementById("inItScreen").innerHTML = "Stop Sharing";
+    // Update the screen sharing state.
+    isSharingEnabled.value = true;
+  } else {
+    // Replace the screen track with the local video track.
+    await channelParameters.screenTrack.replaceTrack(
+      channelParameters.localVideoTrack,
+      true
+    );
+    // Update the button text.
+    document.getElementById("inItScreen").innerHTML = "Share Screen";
+    // Update the screen sharing state.
+    isSharingEnabled.value = false;
+  }
+};
+</script>
 <template>
   <div>
-    <button type="button" id="inItScreen">Share Screen</button>
+    <video
+      ref="localVideo"
+      autoplay
+      style="height: 200px; width: 200px"
+    ></video>
+    <button type="button" id="inItScreen" @click="handleClick">
+      Share Screen
+    </button>
     <button type="button" id="muteVideo">Mute Video</button>
     <br /><label> Local Audio Level :</label>
     <input
